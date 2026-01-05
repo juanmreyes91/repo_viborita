837ebc94c3f462807f75b3f2f214aacac49bf31e
Juan Manuel Reyes Lima <juanmreyes91@gmail.com>
2026-01-05 12:27:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pygame
+pygame.init()
 
+#Constantes
+FPS = 10
+ANCHO = 400
+ALTO = 360
+
+window = pygame.Display.set_mode((ANCHO, ALTO))
+clock = pygame.time.Clock()
+run = True
+
+
+while run:
+  clock.tick(FPS)
+  for events in pygame.
