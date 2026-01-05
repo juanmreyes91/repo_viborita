a2cd9c7e5f276cba349c94c6e0034f44ba57fe7a
juanmreyes91 <juanmreyes91@gmail.com>
2026-01-05 12:40:19 -0300

    Update main.py
    Base del juego:
    - Inicializa pygame
    - Fondo de pantalla negro
    - Cuadrado verde representando a la víbora
    - Actualización de pantalla

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ FPS = 10
 ANCHO = 400
 ALTO = 360
 
-window = pygame.Display.set_mode((ANCHO, ALTO))
+window = pygame.display.set_mode((ANCHO, ALTO))
 clock = pygame.time.Clock()
 run = True
+direction = (10, 0)
+
+snake = pygame.Rect((0, 0),(10, 10))
+colour_snake = pygame.Surface((10, 10))
+colour_snake.fill((0, 255, 0))
 
 
 while run:
   clock.tick(FPS)
-  for events in pygame.
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP:
+        pass
+      if event.key == pygame.K_DOWN:
+        pass
+      if event.key == pygame.K_RIGHT:
+        pass
+      if event.key == pygame.K_LEFT:
+        pass
+  window.fill((0, 0, 0))
+  window.blit(colour_snake, snake)
+  pygame.display.update()
+
+pygame.quit()
